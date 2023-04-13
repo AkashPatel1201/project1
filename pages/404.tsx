a491ca76057218a7d89d1fd0c2525a68331a1734
AkashPatel1201 <akashchitrodi@gmail.com>
2023-04-13 17:36:48 +0530

    #

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { Header } from '@components/Header';
+import { Header } from '@components/Header/index';
 
 const ErrorPage = ():JSX.Element => {
     const router = useRouter();
